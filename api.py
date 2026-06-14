39f027a8f12d1f87b4dafb950f2ff5834a8bd87c
Devansh <devanshmittal271@gmail.com>
2026-06-14 17:40:18 +0530

    Add CORS middleware

--- api.py
+++ api.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware 
 from app import ask
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Question(BaseModel):
     question: str
